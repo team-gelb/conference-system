94ff9c87a73d64342f97cdabf195f85fac8b90ab
j-jimmny <138939114+j-jimmny@users.noreply.github.com>
2025-03-10 16:20:43 +0000

    Dynamic Route Test

---
+++ calls/functions/[users].js
@@ -0,0 +1,10 @@
+export function onRequest(context) {
+    const registeredUsers = [
+        "Flynn",
+        "Jacob"
+    ];
+
+    const checkUserIsRegistered = registeredUsers.includes(context.params.users)
+
+    return new Response("Der Nutzer" + context.params.users + " ist" + (checkUserIsRegistered ? "" : " nicht") + " registriert.");
+}
