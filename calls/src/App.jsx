6a21942a3bc693c92468e8be53b7f89b1c322629
j-jimmny <138939114+j-jimmny@users.noreply.github.com>
2025-03-11 16:48:16 +0100

    Update App.jsx

---
+++ calls/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function CloudflareWorkerComponent() {
+  const [data, setData] = useState(null);
+  const [input, setInput] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://team-gelb.de/${input}`);
+      if (!response.ok) {
+        throw new Error("Fehler beim Abrufen der Daten");
+      }
+      const result = await response;
+      setData(result);
+    } catch (error) {
+      console.error("Fehler:", error);
+      setData("Fehler beim Laden der Daten.");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Gib einen Wert ein..."
+      />
+      <button onClick={fetchData}>Daten abrufen</button>
+      <div>{data ? data : "Noch keine Daten geladen."}</div>
+    </div>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <CloudflareWorkerComponent />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
